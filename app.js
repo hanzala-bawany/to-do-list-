8207846b8217fbbed606abcbef21ff25c94c09e3
hanzala bawany <hanzalabawany0@gmail.com>
2024-12-22 12:40:11 +0500

    js file changes

--- app.js
+++ app.js
@@ -8,14 +8,54 @@ function onSubmit() {
     div.classList.add("additionOfInput");
     div.innerHTML = ` <p class="input">${userInput.value}</p>
     <div class="btns">
-        <button class="remove">Remove</button>
-        <button class="edit">Edit</button>
+        <button onclick="remove(this.parentElement)" class="remove">Remove</button>
+        <button onclick="edit(this.parentElement)" class="edit">Edit</button>
     </div>`
-    contaioner_which_take_inputs.appendChild(div);
-
+    contaioner_which_take_inputs.prepend(div);
 
     userInput.value = ""
-    
-    console.log(userInput);
-
 }
+
+function remove(removeItem) {
+
+    let remove = removeItem.parentElement;
+    remove.remove()
+    console.log(remove, " --> this is remove item");
+}
+
+let subBtn = document.body.querySelector("#subBtn");
+
+function edit(editItem) {
+    let userInput = document.body.querySelector("#userInput");
+    let editBox = editItem.parentElement;
+    let editItemKaInput = editBox.querySelector(".input");
+
+
+    userInput.value = editItemKaInput.innerHTML;
+
+    editItemKaInput.innerHTML = "";
+
+    subBtn.innerHTML = "Edit Submit";
+    subBtn.setAttribute("onclick", "onEditSubmit()");
+
+    editBox.classList.add("forEdit")
+    console.log(editBox,"when add class");
+}
+
+function onEditSubmit() {
+    let jisMeInputBhejnaHe = document.body.querySelector(".forEdit")
+    let userInput = document.body.querySelector("#userInput");
+   let jisMeInputBhejnaHeUsKaInput =  jisMeInputBhejnaHe.querySelector(".input");
+    
+   jisMeInputBhejnaHeUsKaInput.innerHTML = userInput.value;
+    
+    userInput.value = "";
+
+    subBtn.innerHTML = "Submit";
+    subBtn.setAttribute("onclick", "onSubmit()");
+
+    jisMeInputBhejnaHe.classList.remove("forEdit");
+    console.log(jisMeInputBhejnaHe,"when remove class");
+}
+
+
